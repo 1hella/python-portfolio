2fbbe53171f00d29bb626bcea32ff7871291bf1f
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-05-15 00:43:49 -0700

    Update contact form and email

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -4,9 +4,10 @@ from send_email import send_email
 st.title("Contact Me")
 
 with st.form(key="email_forms"):
+    name = st.text_input("Your name")
     email_address = st.text_input("Your email address")
     message = st.text_area("Your message")
     button = st.form_submit_button("Submit")
     if button:
-        send_email(email_address, message)
+        send_email(email_address, message, name)
         st.info("Email sent")
--- send_email.py
+++ send_email.py
@@ -5,16 +5,24 @@ import os
 import certifi
 
 
-def send_email(email_address, message):
+def send_email(email_address, message, name):
     load_dotenv()
     username = os.getenv("EMAIL_USERNAME")
     password = os.getenv("EMAIL_PASSWORD")
+    to_address = os.getenv("TO_EMAIL_ADDRESS")
 
     host = "smtp.gmail.com"
     port = 465
 
     context = ssl.create_default_context(cafile=certifi.where())
 
+    my_message = f"Subject: New email\r\n" \
+                 f"From: {username}\r\n" \
+                 f"To: {to_address}\r\n\r\n" \
+                 f"New message from {name} at {email_address}: " \
+                 f"{message}"
+
+    print(my_message)
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, email_address, message)
+        server.sendmail(username, to_address, my_message)
