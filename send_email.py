3dcc99e9c1bc86ad8bd13d8e3f5bbfbce201f118
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-05-14 23:09:09 -0700

    update send_email.py

--- send_email.py
+++ send_email.py
@@ -2,17 +2,18 @@ import smtplib
 import ssl
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-username = os.getenv("EMAIL_USERNAME")
-password = os.getenv("EMAIL_PASSWORD")
+import certifi
 
 
 def send_email(email_address, message):
+    load_dotenv()
+    username = os.getenv("EMAIL_USERNAME")
+    password = os.getenv("EMAIL_PASSWORD")
+
     host = "smtp.gmail.com"
     port = 465
 
-    context = ssl.create_default_context()
+    context = ssl.create_default_context(cafile=certifi.where())
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
