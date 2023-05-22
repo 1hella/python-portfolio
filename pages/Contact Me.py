4d25caa5e8b8cc83bb4b00ccb2b48437ce93f696
Stephen Wanhella <Stephen.wanhella@gmail.com>
2023-05-22 10:21:18 -0700

    Reformat file

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -10,14 +10,17 @@ with st.form(key="email_forms", clear_on_submit=True):
         st.session_state['submitted'] = False
 
     name = st.text_input("Your name", key="name",
-                         value=st.session_state['name'] if 'name' in st.session_state and not st.session_state['submitted'] else '')
+                         value=st.session_state['name'] if 'name' in st.session_state and not st.session_state[
+                             'submitted'] else '')
     name_error_message = st.empty()
     email_address = st.text_input("Your email address", key="email",
-                                  value=st.session_state['email'] if "email" in st.session_state and not st.session_state['submitted']
-    else "")
+                                  value=st.session_state['email'] if "email" in st.session_state and not
+                                  st.session_state['submitted']
+                                  else "")
     email_error_message = st.empty()
     message = st.text_area("Your message", key="message",
-                           value=st.session_state['message'] if "message" in st.session_state and not st.session_state['submitted'] else "")
+                           value=st.session_state['message'] if "message" in st.session_state and not st.session_state[
+                               'submitted'] else "")
     message_error_message = st.empty()
     submit = st.form_submit_button("Submit")
     if st.session_state['submitted']:
